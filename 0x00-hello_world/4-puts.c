1c114354bc2fc83a82c969eae82d47ad0410ef37
benardlevy <benardlevy1@gmail.com>
2022-10-14 07:13:28 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main - Entry
  * Return: Always 0
  */
-int min (void)
-{
-	put("Programming is like building a multilingual puzzle\n");
-	return 0;
-}
+int main (void)
+
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+
 
