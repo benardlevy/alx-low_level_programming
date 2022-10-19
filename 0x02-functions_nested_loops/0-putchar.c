2bbca11c43f128def06d637a64d4244b58c0fe2c
benardlevy <benardlevy1@gmail.com>
2022-10-19 02:45:48 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and a new line 
- * Return: Always 0
+ * main - print _putchar
+ * Return: 0
  */
 int main(void)
 {
@@ -16,5 +16,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return (0 ,);
+	return (0);
 }
