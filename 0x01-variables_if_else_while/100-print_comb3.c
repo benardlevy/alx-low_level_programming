a1c4ba552d6d9b2922abdc6e784c0511b2329bfa
benardlevy <benardlevy1@gmail.com>
2023-06-18 01:51:56 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,15 +11,15 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 0; m < 9; n++)
+	for (n = '0'; m < '9'; n++)
 	{
-	for (m = n + 1; m <= 9; m++)
+	for (m = n + 1; m <= '9'; m++)
 	{
 	if (m != n)
 	{
 	putchar(n);
 	putchar(m);
-	if (n == 8 && m == 9)
+	if (n == '8' && m == '9')
 	continue;
 	putchar(',');
 	putchar(' ');
