115dab895a7630afd4a970323869f4c026761022
benardlevy <benardlevy1@gmail.com>
2023-06-21 06:26:24 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,10 +5,16 @@
  * @c: the char to be checked
  * Return: 1 if char is letter lower or upper, 0 otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
-		return (1);
+	if ((c >= 97 && c <= 122) || (c >= 65  && <= 90))
+	{
+	return (1);
+	}
 	else
-		return (0);
+	{
+	return (0);
+	}
+	_putchar('\n');
 }
+		
