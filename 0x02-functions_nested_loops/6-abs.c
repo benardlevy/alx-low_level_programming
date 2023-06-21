b26b96eb6b8397d61a1185f66d9c8a7cdcb912a8
benardlevy <benardlevy1@gmail.com>
2023-06-21 08:23:06 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,9 +6,14 @@
  * Return: 0
  */
 
-int _abs(int n);
+int _abs(int n)
 {
-	if (n >= 0)
-		return (n);
-	else
-		return (-n);
+	if (n < 0)
+	{
+	int abs_val;
+
+	abs_val = n * -1;
+	return (abs_val);
+	}
+	return (n);
+}
