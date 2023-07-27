265d0dfa756311e42d3ce16656b0717f9fe79d04
benardlevy <benardlevy1@gmail.com>
2023-07-27 01:58:46 -0700

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void first(void) _attribute_ ((constructor));
+
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ * Return: Always 0
+ */
+
+void first(void)
+{
+
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+
+}
